6b81c56a9cfb88478e34992ab837a047fd1fc9fd
toy-panda <mems.imperia@gmail.com>
2025-01-26 21:57:46 +0200

    Add files via upload

---
+++ modules/islands.js
@@ -0,0 +1,38 @@
+export const islands = {
+    WrightIsles: {
+        img: document.getElementById("wright"),
+        scale: {
+            width: 2.34979,
+            height: 2.3369
+        },
+        offset: {
+            x: -13736+6019.41,
+            y: -13163+5594
+        },
+        name: "Wright Isles"
+    },
+    Krakabloa: {
+        img: document.getElementById("krakabloa"),
+        scale: {
+            width: 2.3406,
+            height: 2.3446
+        },
+        offset: {
+            x: -13532+26392,
+            y: -9338-53185
+        },
+        name: "Krakabloa"
+    },
+    SkyPark: {
+        img: document.getElementById("skypark"),
+        scale: {
+            width: 5.306,
+            height: 5.306
+        },
+        offset: {
+            x: -1372-679,
+            y: -937+31029
+        },
+        name: "SkyPark City"
+    },
+}
