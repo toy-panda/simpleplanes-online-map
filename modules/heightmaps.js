722fb31edebee6f1f7465713635fe2971cbb2922
toy-panda <mems.imperia@gmail.com>
2025-03-16 12:58:02 +0200

    Add files via upload

---
+++ modules/heightmaps.js
@@ -0,0 +1,14 @@
+export const heightmaps = {
+    Sample: {
+        img: document.getElementById("krakabloa-h"),
+        scale: {
+            width: 8.93522,
+            height: 8.92638
+        },
+        offset: {
+            x: -1383+14338,
+            y: -3395-58941
+        },
+        name: "Sample"
+    },
+}
